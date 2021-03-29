4fbc538c84cd2ce1cb633cace12b6ab858e11295
Daan Zimmerman van Woesik <daan.zimmerman@gmail.com>
2021-03-30 01:13:05 +0200

    Replaced the ICO in the app dir
    Replaced the ICO file in a more understandable directory

--- test_module.py
+++ test_module.py
@@ -7,7 +7,7 @@ from app.lib.gui import gui
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Encrypter")
-    root.iconbitmap('res/gui/encrypter-logo.ico')
+    root.iconbitmap('app/res/encrypter-logo.ico')
     gui_class = gui(master=root)
     gui_class.mainloop()
 
